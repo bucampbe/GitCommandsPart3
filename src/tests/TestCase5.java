f1a8d39ed378b85eb63a98321e7c332fc1ebd7b7
bucampbe <161543471+bucampbe@users.noreply.github.com>
2024-09-23 12:31:42 -0400

    Update TestCase5.java

--- src/tests/TestCase5.java
+++ src/tests/TestCase5.java
@@ -5,4 +5,7 @@ public class TestCase5 {
     //this is test case 5
     //TODO: add more test cases
     //TODO: write a test case to verify the title of the webpage
+
+    //more testcase on this file
+    //I added more methods
 }
