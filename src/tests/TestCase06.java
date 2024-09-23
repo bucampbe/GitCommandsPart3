212ea8cd18540a9a2c4a0e6d756510de549e627a
bucampbe <btakyrbashova@gmail.com>
2024-09-23 11:30:41 -0400

    TestCase06.java

---
+++ src/tests/TestCase06.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class TestCase06 {
+
+    //this is test case 6
+    //add more test cases here
+    //test case 06: Verify that the password strength indicator shows weak for a password with less than 8 characters, medium for a password with 8-12 characters, and strong for a password with more than 12 characters
+}
